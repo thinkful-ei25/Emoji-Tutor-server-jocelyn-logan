cd1cad2fe80c0732612db4510041023f9e9ec12e
jsantiag <jsantiag@wellesley.edu>
2019-01-07 11:15:40 -0600

    lil bugs fixed

---
+++ routes/users.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const express = require('express');
+
+const {User} = require('../models/user');
+
+
+const router = express.Router();
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  User.findOne({_id: id})
+    .then(result => {
+      if(result){
+        res.json(result);
+      }else{
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.get('/', (req, res, next) => {
+
+  User.find() .sort('name') .then(results => { res.json(results); }) 
+    .catch(err => { next(err); }); });
+
+/* ========== POST/CREATE AN ITEM ========== */
+router.post('/', (req, res, next) => {
+  let {username, password}= req.body;
+
+  return User.hashPassword(password)
+    .then(digest => {
+      const newUser = {
+        username,
+        password: digest,
+      };
+      return User.create(newUser);
+    })
+    .then(result => {
+      return res.status(201).location(`/api/users/${result.id}`).json(result);
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The username already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
+
+module.exports = router;
